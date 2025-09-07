1bfaccbd403f0057f14cc8c4db6dc952ce0972df
frontpage656 <joshuasimon656@gmail.com>
2025-09-07 11:58:44 +0300

    Initial project setup

---
+++ src/main/java/org/nextgate/nextgatebackend/NexgateBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nextgate.nextgatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexgateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexgateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nextgate/nextgatebackend/NexgateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nextgate.nextgatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexgateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
