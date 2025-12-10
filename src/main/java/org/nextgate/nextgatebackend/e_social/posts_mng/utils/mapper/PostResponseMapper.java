753278efeda3da9567bb07e43c918118acaa00e1
frontpage656 <joshuasimon656@gmail.com>
2025-12-10 21:40:17 +0300

    fixed stallement plan respose in post

--- src/main/java/org/nextgate/nextgatebackend/e_social/posts_mng/utils/mapper/PostResponseMapper.java
+++ src/main/java/org/nextgate/nextgatebackend/e_social/posts_mng/utils/mapper/PostResponseMapper.java
@@ -560,7 +560,7 @@ public class PostResponseMapper {
 
     private String getFulfillmentDisplay(FulfillmentTiming timing) {
         return switch (timing) {
-            case  AFTER_PAYMENT  -> "Ships after down payment";
+            case  AFTER_PAYMENT  -> "Ships after full payment";
             case IMMEDIATE -> "Ships immediately";
         };
     }
